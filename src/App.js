24505307836bc4a407c10039c1698987ee19486f
Emiko Esther S <graceupongraceemi@gmail.com>
2022-11-04 14:01:49 -0700

    getRamdomPage and useGetImages functions moved to useGetImages.js hook

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import Background from './components/Background';
 
+import useGetImages from './hooks/useGetImages';
+
+// const getRandomPage = () => Math.round(Math.random() * (10 - 1) + 1);
+
+// const BASE_URL = 'https://api.pexels.com/v1/search?query=nature';
+
 function App() {
+  useGetImages();
+
   return (
     <>
       <Background />
@@ -10,3 +18,22 @@ function App() {
 }
 
 export default App;
+
+// const buildUrl = () => {
+//   let url = new URL('https://api.pexels.com/v1/search');
+
+//   url.search = new URLSearchParams({
+//     query: 'nature', // TODO: change to a variable
+//     orientation: 'square',
+//     size: 'small',
+//     per_page: 2, // TODO: change to a variable
+//     page: getRandomPage()
+//   });
+
+//   return url;
+// };
+
+// fetch(buildUrl(), {
+//   headers: {
+//     Authorization: process.env.REACT_APP_AUTH_KEY
+//   }
--- src/hooks/useGetImages.js
+++ src/hooks/useGetImages.js
@@ -1,5 +1,27 @@
 const BASE_URL = 'https://api.pexels.com/v1/search';
 
-const useGetImages = () => {};
+const getRandomPage = () => Math.round(Math.random() * (10 - 1) + 1);
+
+const useGetImages = () => {
+  const buildUrl = () => {
+    let url = new URL('https://api.pexels.com/v1/search');
+
+    url.search = new URLSearchParams({
+      query: 'nature', // TODO: change to a variable
+      orientation: 'square',
+      size: 'small',
+      per_page: 2, // TODO: change to a variable
+      page: getRandomPage()
+    });
+
+    return url;
+  };
+
+  fetch(buildUrl(), {
+    headers: {
+      Authorization: process.env.REACT_APP_AUTH_KEY
+    }
+  });
+};
 
 export default useGetImages;
