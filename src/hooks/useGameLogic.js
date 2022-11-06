7cac0295cbdcbcc648ce9aeb9eab09a31d93a09d
Emiko Esther S <graceupongraceemi@gmail.com>
2022-11-06 10:21:53 -0800

    In the useGameLogic.js, shuffleCards in the useGameLogic function is defined as the main as well as uniqueId is added in the function as well

--- src/hooks/useGameLogic.js
+++ src/hooks/useGameLogic.js
@@ -1,15 +1,38 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import {
+  addUniqueIds,
+  getFormedData,
+  getPairedPics,
+  shuffleCards
+} from '../utils';
+
+// [
+//   {
+//     id: // to match the pair, can be just the index of it
+//     uri: // to display the img
+//     isShown: // to show the card of the image
+//     isFound: // to know if a match was found
+//   }
+// ]
 
 const useGameLogic = (images) => {
+  const [cards, setCards] = useState([]);
+
   const prepareCards = () => {
-    console.log(images);
+    const a = getFormedData(images);
+    const b = getPairedPics(a);
+    const c = addUniqueIds(b);
+    const d = shuffleCards(c);
+    setCards(d);
   };
 
   useEffect(() => {
     if (images.length > 0) prepareCards();
   }, [images]);
+
+  return cards;
 };
 
 export default useGameLogic;
