022d41ea51273d25a0376f37cd75a6c1735177cc
Emiko Esther S <graceupongraceemi@gmail.com>
2022-11-07 15:30:09 -0800

    Card.jsx updated with Card propTypes

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -25,3 +25,13 @@ const Card = ({ card, onCardClick }) => {
 };
 
 export default Card;
+
+Card.propTypes = {
+  card: PropTypes.shape({
+    url: PropTypes.string.isRequired,
+    uniqueId: PropTypes.string.isRequired,
+    isShown: PropTypes.bool.isRequired,
+    isFound: PropTypes.bool.isRequired
+  }),
+  onCardClick: PropTypes.func.isRequired
+};
